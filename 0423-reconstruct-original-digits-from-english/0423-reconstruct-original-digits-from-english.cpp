0280d7423827af234b4e93e941b1809dc025fe24
THIRUMURUGAN A <thirumurugan936348@gmail.com>
2026-03-12 18:02:26 +0530

    Time: 8 ms (17.95%), Space: 11.3 MB (88.64%) - LeetHub

--- 0423-reconstruct-original-digits-from-english/0423-reconstruct-original-digits-from-english.cpp
+++ 0423-reconstruct-original-digits-from-english/0423-reconstruct-original-digits-from-english.cpp
@@ -2,50 +2,46 @@ class Solution {
 public:
     string originalDigits(string s) {
         map<char,int> mp;
+
         for(char c:s)
-        {
-            char xx=tolower(c);
-            mp[xx]++;
-        }
+            mp[c]++;
+
         vector<int> nums(10,0);
         string nus[10]={"zero","one","two","three","four","five","six","seven","eight","nine"};
-        for(int i=0;i<10;i++)
+
+        int order[10]={0,2,4,6,8,3,5,7,1,9};
+
+        for(int x=0;x<10;x++)
         {
+            int i=order[x];
+
             int mini=INT_MAX;
             string number=nus[i];
-            for(int j=0;j<number.length();j++)
-            {
-                char firstchar=number[j];
-                mini=min(mini,mp[firstchar]);
-            }
-            if(mini==0)
-            {
-                continue;
-            }
-            nums[i]=mini;
-            for(int k=0;k<number.size();k++)
-            {
-                char f=number[k];
-                mp[f]-=mini;
-                if(mp[f]==0)
-                {
-                    mp.erase(f);
-                }
-            }
 
+            for(char c:number)
+                mini=min(mini,mp[c]);
+
+            if(mini==0)
+                continue;
+
+            nums[i]=mini;
+
+            for(char c:number)
+            {
+                mp[c]-=mini;
+                if(mp[c]==0)
+                    mp.erase(c);
+            }
         }
 
         string ans="";
+
         for(int i=0;i<10;i++)
         {
-            if(nums[i]!=0)
-            {
-                for(int k=0;k<nums[i];k++)
-                {
-                    ans+=(char)(i+'0');
-                }
-            }
+            for(int k=0;k<nums[i];k++)
+                ans+=char(i+'0');
         }
-     return ans;
+
+        return ans;
     }
 };
