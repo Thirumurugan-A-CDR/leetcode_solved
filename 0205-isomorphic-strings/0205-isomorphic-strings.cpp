7721eed389041333d3842c8f073367b465a18e48
THIRUMURUGAN A <thirumurugan936348@gmail.com>
2026-03-27 12:13:39 +0530

    Time: 4 ms (22.7%), Space: 9.3 MB (54.84%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        map<char,char> mp;
+        for(int i=0;i<s.length();i++)
+        {
+            if(mp.find(s[i])==mp.end())
+            {
+                mp.insert({s[i],t[i]});
+            }
+            else
+            {
+                if(mp[s[i]]!=t[i])
+                {
+                    return false;
+                }
+            }
+        }
+        mp.clear();
+        for(int i=0;i<s.length();i++)
+        {
+            if(mp.find(t[i])==mp.end())
+            {
+                mp.insert({t[i],s[i]});
+            }
+            else
+            {
+                if(mp[t[i]]!=s[i])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
