138450cad8eda64f62fd4ecd0d2aac2681fa82eb
THIRUMURUGAN A <thirumurugan936348@gmail.com>
2026-03-28 12:16:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3619-count-islands-with-total-value-divisible-by-k/3619-count-islands-with-total-value-divisible-by-k.cpp
+++ 3619-count-islands-with-total-value-divisible-by-k/3619-count-islands-with-total-value-divisible-by-k.cpp
@@ -1,41 +1,45 @@
 class Solution {
 public:
     int countIslands(vector<vector<int>>& grid, int k) {
-  int n = grid.size();
-        int m = grid[0].size();
+        int n = grid.size(), m = grid[0].size();
         int count = 0;
-        int sum=0;
-        vector<vector<int>> visited(n, vector<int>(m, -1));
-        vector<int> dr = {-1, 0, 1, 0};
-        vector<int> dc = {0, 1, 0, -1};
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                queue<pair<int, int>> q;
-                if (grid[i][j] !=0 && visited[i][j]==-1) {
-                    visited[i][j] = 1;
-                    q.push({i, j});
-                     sum=0;
-                    while (!q.empty()) {
-                        auto it = q.front();
-                        q.pop();
-                        int r = it.first;
-                        int c = it.second;
-                        sum=(sum+grid[r][c])%k;
-                        for (int i = 0; i < 4; i++) {
-                            int nr = r + dr[i];
-                            int nc = c + dc[i];
 
-                            if (nr >= 0 && nc >= 0 && nr < n && nc < m &&
-                                visited[nr][nc] == -1 && grid[nr][nc] !=0) {
-                                visited[nr][nc] = 1;
-                                q.push({nr, nc});
+        vector<vector<char>> vis(n, vector<char>(m, 0));
+        int dr[4] = {-1, 0, 1, 0};
+        int dc[4] = {0, 1, 0, -1};
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+
+                if(grid[i][j] != 0 && !vis[i][j]) {
+
+                    queue<pair<int,int>> q;
+                    q.emplace(i, j);
+                    vis[i][j] = 1;
+
+                    int sum = 0;
+
+                    while(!q.empty()) {
+                        auto [r, c] = q.front();
+                        q.pop();
+
+                        sum += grid[r][c];
+                        if(sum >= k) sum %= k;
+
+                        for(int d = 0; d < 4; d++) {
+                            int nr = r + dr[d];
+                            int nc = c + dc[d];
+
+                            if(nr >= 0 && nc >= 0 && nr < n && nc < m &&
+                               !vis[nr][nc] && grid[nr][nc] != 0) {
+
+                                vis[nr][nc] = 1;
+                                q.emplace(nr, nc);
                             }
                         }
                     }
-                    if(sum%k==0)
-                    {
-                        count++;
-                    }
+
+                    if(sum % k == 0) count++;
                 }
             }
         }
