3c85a5359aedb32b9226c2d572fe4bf38005b774
THIRUMURUGAN A <thirumurugan936348@gmail.com>
2026-04-05 16:38:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -3,18 +3,17 @@ public:
     bool judgeCircle(string moves) {
         int x=0;
         int y=0;
-        int n=moves.length();
-        for(int i=0;i<n;i++)
+        for(char c:moves)
         {
-            if(moves[i]=='U')
+            if(c=='U')
             {
                 y+=1;
             }
-            else if(moves[i]=='D')
+            else if(c=='D')
             {
                 y-=1;
             }
-            else if(moves[i]=='L')
+            else if(c=='L')
             {
                 x-=1;
             }
