2bc0cbde3bfb5dd9ff8448d3b6b8cb89939cd25c
THIRUMURUGAN A <thirumurugan936348@gmail.com>
2026-06-05 09:33:09 +0530

    Time: 0 ms (100%), Space: 15 MB (54.74%) - LeetHub

---
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+       int ans=0;
+        for(int i=0;i<mat.size();i++)
+        {
+                    ans+=mat[i][i];
+        }
+        int j=mat.size()-1;
+        for(int  i=0;i<mat.size();i++)
+        {
+                ans+=mat[i][j];
+                j--;
+        }
+        if(mat.size()%2==1)
+        {
+            ans-=mat[mat.size()/2][mat.size()/2];
+        }
+        return ans;
+    }
+};
