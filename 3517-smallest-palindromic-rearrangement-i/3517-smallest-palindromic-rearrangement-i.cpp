36a66615fa34a182046482863b6efffd3d3d401d
THIRUMURUGAN A <thirumurugan936348@gmail.com>
2026-07-28 09:21:22 +0530

    Time: 115 ms (23.3%), Space: 75.7 MB (34.94%) - LeetHub

---
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        if (s.length() == 1) return s;
+
+        int n = s.length();
+        int half = n / 2;          // Only take the first half
+        string firsthalf = "";
+
+        for (int i = 0; i < half; i++) {
+            firsthalf += s[i];
+        }
+
+        sort(firsthalf.begin(), firsthalf.end());
+
+        string temp = firsthalf;
+
+        if (n % 2 != 0) {
+            temp += s[n / 2];
+        }
+
+        reverse(firsthalf.begin(), firsthalf.end());
+
+        temp += firsthalf;
+
+        return temp;
+    }
+};
