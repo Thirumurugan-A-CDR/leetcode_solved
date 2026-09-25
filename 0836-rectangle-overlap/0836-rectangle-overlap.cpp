3edce25d9c4bd89e45baef8cdb8afdea96c3ea0b
THIRUMURUGAN A <thirumurugan936348@gmail.com>
2026-09-25 20:09:36 +0530

    Time: 0 ms (100%), Space: 9.8 MB (49.76%) - LeetHub

--- 0836-rectangle-overlap/0836-rectangle-overlap.cpp
+++ 0836-rectangle-overlap/0836-rectangle-overlap.cpp
@@ -12,9 +12,6 @@ public:
         int a2 = rec2[2];
         int b2 = rec2[3];
 
-        // Check if one rectangle is completely
-        // to the left, right, above, or below the other
-
         if (x2 <= a1 || a2 <= x1 ||
             y2 <= b1 || b2 <= y1) {
             return false;
